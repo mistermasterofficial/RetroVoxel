328cdaf98d7be06d2dd5e83d31621d59a0dfb7a8
MrMaster <106398602+mistermasterofficial@users.noreply.github.com>
2023-11-19 21:03:22 +0700

    clear

--- thread.py
+++
@@ -1,8 +0,0 @@
-from threading import *
-
-def thread(fn):
-    def wrapper(*args, **kwargs):
-        thread = Thread(target=fn, args=args, kwargs=kwargs)
-        thread.start()
-        return thread
-    return wrapper
